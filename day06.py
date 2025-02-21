7c95d35a19c41f7bb9848e126f828320fc4e23cb
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2025-02-22 03:26:12 +0900

    v0.3 day06 is_even

--- day06.py
+++ day06.py
@@ -4,16 +4,7 @@ def is_even(n):
     :param n: 판정할 정수
     :return: 짝수면 True, 홀수면 False
     """
-    if n % 2 == 0:
-        return True
-    return False
-
-a = 10
-b = 11
-
-print(a & b)
-print(a | b)
-print(a ^ b)
+    return not n & 1
 
 n = int(input())
 print(is_even(n))
