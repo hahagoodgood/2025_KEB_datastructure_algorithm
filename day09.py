5412c0234296b9908ba131415bcd3f359dda8e85
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2025-02-22 13:57:39 +0900

    v3.6 day09 tree

--- day09.py
+++ day09.py
@@ -28,7 +28,21 @@ def insert(root, value):
 
 
 def search(root, value):
-    pass
+    current = root
+    while True:
+        if value == current.data:
+            print(f"{value}을(를) 찾았습니다")
+            break
+        elif value < current.data:
+            if current.left is None:
+                print(f"{value}이(가) 존재하지 않습니다")
+                break
+            current = current.left
+        else:
+            if current.right is None:
+                print(f"{value}이(가) 존재하지 않습니다")
+                break
+            current = current.right
 
 
 def post_order(node):
@@ -49,4 +63,6 @@ if __name__ == "__main__":
     print("BST 구성 완료")
     post_order(root)
 
-
+    print()
+    find_number = int(input())
+    search(root, find_number)
